5e6facefba57feac92f2d6e36391274d44849ec8
AizenSanchez <asancheza@fi.uba.ar>
2024-04-18 19:03:17 -0300

    UpdateNuevoCuerpo

--- tp1_fig_san/src/main/java/robot/Modelo/Acciones/AccionTeleportRandom.java
+++ tp1_fig_san/src/main/java/robot/Modelo/Acciones/AccionTeleportRandom.java
@@ -1,10 +1,12 @@
 package robot.Modelo.Acciones;
 
 import robot.Modelo.EstadoDeJuego;
+import robot.Modelo.Vec2D;
+
+import java.util.Random;
 
 public class AccionTeleportRandom implements Action{
     @Override
     public void aplicar(EstadoDeJuego e) {
-
     }
 }
